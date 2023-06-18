8ba0eda7ecbe5422603c34cddac96eb7e8c24220
Bola Komolafe <kobol11@yahoo.com>
2023-06-17 20:36:43 -0400

    Add static file and routing functionality

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ import data from "./data/mock.json" assert { type: "json" };
 const app = express();
 
 let PORT = process.env.PORT || 3000;
+app.use(express.static("public"));
 
 app.get("/api", (req, res) => {
   res.send(data);
 });
 
-app.put("/api/update", (req, res) => {
-  res.send("This is a PUT request at /update");
-});
-
-app.post("/api/create", (req, res) => {
-  res.send("This is a POST request at /create");
-});
-
-app.delete("/api/delete", (req, res) => {
-  res.send("This is a DELETE request at /delete");
-});
+app
+  .route("/api/user/:id")
+  .get((req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = data.find((user) => user.id === userId);
+    res.send(user);
+  })
+  .put((req, res) => {
+    res.send("This is a PUT request at /api/user/:id");
+  })
+  .post((req, res) => {
+    res.send("This is a POST request at /api/user/:id");
+  })
+  .delete((req, res) => {
+    res.send("This is a DELETE request at /api/user/:id");
+  });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
